54d6eade7c22625c57f27b718802226e6c8613cf
Varun Raj <varunraj82@gmail.com>
2020-08-11 23:10:18 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express') 
+
+const app = express();
+
+console.log(app.get)
+
+
+app.get('/',(req, res)=>{
+    res.send({hi:'there'})
+})
+
+
+// Inject port as env variable.In the heroku deployment, it is assigned
+// by env variable. In local, we will use 5000
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
